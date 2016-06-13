870273cdd705597099dddd4f93b1c0270aa0b2a2
Radium Zheng <ztl8702@msn.com>
2016-06-13 15:06:57 +1000

    Refactor BlockRenderer

--- src/Render/MainContent/BlockRenderer.php
+++ src/Render/MainContent/BlockRenderer.php
@@ -36,34 +36,33 @@ class BlockRenderer implements MainContentRendererInterface {
    */
   protected $htmlRenderer;
 
+  protected $blocks;
+  protected $page_attachments;
+  protected $page;
+
   /**
    * WebComponentRenderer constructor.
    * @param MainContentRendererInterface $html_renderer
    */
   public function __construct(MainContentRendererInterface $html_renderer) {
     $this->htmlRenderer = $html_renderer;
-    // Modified version of the core "renderer" service
     $this->renderer = \Drupal::service('renderer');
+
+    $this->blocks = [];
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function renderResponse(array $main_content, Request $request, RouteMatchInterface $route_match) {
-    // URL parameter
-    $block_requested = $request->get("block");
+  protected function prepare(array $main_content, Request $request, RouteMatchInterface $route_match) {
+    // TODO: remove reliance on HTMLRenderer.
+    list($this->page, $title) = $this->htmlRenderer->prepare($main_content, $request, $route_match);
 
-    list($page, $title) = $this->htmlRenderer->prepare($main_content, $request, $route_match);
-
-    $page_attachments = $page['#attached'];
-
-    $blocks = [];
+    // Iterate through all blocks.
     $regions = \Drupal::theme()->getActiveTheme()->getRegions();
     foreach ($regions as $region) {
       if (!empty($page[$region])) {
+        // Non-empty region, iterate the blocks inside it.
         foreach ($page[$region] as $key => $child) {
           if (substr($key,0,1) != '#') {
-            $blocks[$region .'/' .$key] = array (
+            $this->blocks[$region .'/' .$key] = array (
               "id" => $region .'/' .$key,
               "render_array" => $child,
             );
@@ -72,58 +71,81 @@ class BlockRenderer implements MainContentRendererInterface {
 
       }
     }
-    foreach ($blocks as &$block) {
+    // Render each block
+    foreach ($this->blocks as &$block) {
       $block['markup'] = $this->renderer->renderRoot($block['render_array']);
     }
 
     $this->renderer->renderRoot($page);
-    $page_attachments = $page['#attached'];
+    // Save the full assets of the page.
+    $this->page_attachments = $page['#attached'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function renderResponse(array $main_content, Request $request, RouteMatchInterface $route_match) {
+    // Process URL parameters.
+    $block_requested = $request->get("block");
+    // Prepare.
+    $this->prepare($main_content, $request, $route_match);
 
     if (empty($block_requested)) {
       // List Mode
-
-      //  We use a Symfony response object to have complete control over the response.
-      $response = new Response();
-      $response->setStatusCode(Response::HTTP_OK);
-
-      $debug = "";
-      $keys = array_keys($blocks);
-      foreach ($keys as $key) {
-        $debug = $debug. $key . '<br /> ';
-      }
-
-      $response->headers->set('Content-Type', 'text/html');
-      $response->setContent($debug);
-      return $response;
+      return $this->renderBlockList();
     } else {
       //Render Mode
-      $block_to_render = $blocks[$block_requested];
-      if (!empty($block_to_render)) {
-        $render_array = $block_to_render['render_array'];
-        $render_array = $this->renderer->mergeBubbleableMetadata($render_array, $page_attachments);
-        $html = [
-          '#type' => 'wcrhtml',
-          'page' => $render_array,
-          '#attached'=> $page_attachments,
-        ];
-        $html = $this->renderer->mergeBubbleableMetadata($html, $render_array["#cache"]);
-        $html['#cache']['contexts'] = Cache::mergeContexts($html['#cache']['contexts'], [ "url" ]); //url.query_args
-
-        $html ['#cache']['tags'][] = 'rendered';
-        $this->renderer->renderRoot($html);
-        $response = new HtmlResponse($html, 200, [
-          'Content-Type' => 'text/html; charset=UTF-8',
-        ]);
-        return $response;
-      }
-      else {
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-        return $response;
-      }
+      return $this->renderBlock($this->blocks[$block_requested]);
     }
 
+  }
 
+  protected function renderBlockList() {
+    //  We use a Symfony response object to have complete control over the response.
+    $response = new Response();
+    $response->setStatusCode(Response::HTTP_OK);
+
+    $debug = "";
+    $keys = array_keys($this->blocks);
+    foreach ($keys as $key) {
+      $debug = $debug. $key . '<br /> ';
+    }
+
+    $response->headers->set('Content-Type', 'text/html');
+    $response->setContent($debug);
+  }
+
+  protected function renderBlock($block_to_render) {
+    if (!empty($block_to_render)) {
+      $render_array = $block_to_render['render_array'];
+
+      // Merge the assets.
+      $render_array = $this->renderer->mergeBubbleableMetadata($render_array, $this->page_attachments);
+
+      // Use a custom wrapper instead of `html` theme hook.
+      $html = [
+        '#type' => 'wcrhtml',
+        'page' => $render_array,
+        '#attached'=> $this->page_attachments,
+      ];
+      $html = $this->renderer->mergeBubbleableMetadata($html, $render_array["#cache"]);
+      // Add url to cache context, to prevent query arguments being ignored.
+      $html['#cache']['contexts'] = Cache::mergeContexts($html['#cache']['contexts'], [ "url" ]); //url.query_args
+
+      $html ['#cache']['tags'][] = 'rendered';
+
+      //
+      $this->renderer->renderRoot($html);
+      $response = new HtmlResponse($html, 200, [
+        'Content-Type' => 'text/html; charset=UTF-8',
+      ]);
+      return $response;
+    }
+    else {
+      $response = new Response();
+      $response->setStatusCode(Response::HTTP_NOT_FOUND);
+      return $response;
+    }
   }
 
 
