20b8f0e2e07c6c3318226ae78c6bc80a414f71ec
Radium Zheng <ztl8702@msn.com>
2016-06-13 15:25:06 +1000

    Add JSON response format

--- src/Render/MainContent/BlockRenderer.php
+++ src/Render/MainContent/BlockRenderer.php
@@ -58,9 +58,9 @@ class BlockRenderer implements MainContentRendererInterface {
     // Iterate through all blocks.
     $regions = \Drupal::theme()->getActiveTheme()->getRegions();
     foreach ($regions as $region) {
-      if (!empty($page[$region])) {
+      if (!empty($this->page[$region])) {
         // Non-empty region, iterate the blocks inside it.
-        foreach ($page[$region] as $key => $child) {
+        foreach ($this->page[$region] as $key => $child) {
           if (substr($key,0,1) != '#') {
             $this->blocks[$region .'/' .$key] = array (
               "id" => $region .'/' .$key,
@@ -76,9 +76,9 @@ class BlockRenderer implements MainContentRendererInterface {
       $block['markup'] = $this->renderer->renderRoot($block['render_array']);
     }
 
-    $this->renderer->renderRoot($page);
+    $this->renderer->renderRoot($this->page);
     // Save the full assets of the page.
-    $this->page_attachments = $page['#attached'];
+    $this->page_attachments = $this->page['#attached'];
   }
 
   /**
@@ -87,6 +87,7 @@ class BlockRenderer implements MainContentRendererInterface {
   public function renderResponse(array $main_content, Request $request, RouteMatchInterface $route_match) {
     // Process URL parameters.
     $block_requested = $request->get("block");
+    $is_REST = $request->getContentType() == "application/json";
     // Prepare.
     $this->prepare($main_content, $request, $route_match);
 
@@ -94,14 +95,17 @@ class BlockRenderer implements MainContentRendererInterface {
       // List Mode
       return $this->renderBlockList();
     } else {
-      //Render Mode
-      return $this->renderBlock($this->blocks[$block_requested]);
+      // Render Mode
+      if ($is_REST) {
+        return $this->renderBlockREST($this->blocks[$block_requested]);
+      } else {
+        return $this->renderBlock($this->blocks[$block_requested]);
+      }
     }
-
   }
 
   protected function renderBlockList() {
-    //  We use a Symfony response object to have complete control over the response.
+    //  Use a Symfony response object to have complete control over the response.
     $response = new Response();
     $response->setStatusCode(Response::HTTP_OK);
 
@@ -113,6 +117,7 @@ class BlockRenderer implements MainContentRendererInterface {
 
     $response->headers->set('Content-Type', 'text/html');
     $response->setContent($debug);
+    return $response;
   }
 
   protected function renderBlock($block_to_render) {
@@ -143,11 +148,45 @@ class BlockRenderer implements MainContentRendererInterface {
     }
     else {
       $response = new Response();
+      $response->setContent("Block not found.");
       $response->setStatusCode(Response::HTTP_NOT_FOUND);
       return $response;
     }
   }
 
+  protected function renderBlockREST($block_to_render) {
+    if (!empty($block_to_render)) {
+      $render_array = $block_to_render['render_array'];
+
+      // Merge the assets.
+      $render_array = $this->renderer->mergeBubbleableMetadata($render_array, $this->page_attachments);
+
+      // Use a custom wrapper instead of `html` theme hook.
+      $html = [
+        '#type' => 'wcrhtml',
+        'page' => $render_array,
+        '#attached'=> $this->page_attachments,
+      ];
+      $html = $this->renderer->mergeBubbleableMetadata($html, $render_array["#cache"]);
+      // Add url to cache context, to prevent query arguments being ignored.
+      $html['#cache']['contexts'] = Cache::mergeContexts($html['#cache']['contexts'], [ "url" ]); //url.query_args
+
+      $html ['#cache']['tags'][] = 'rendered';
+
+      //
+      $this->renderer->renderRoot($html);
+      $response = new AjaxResponse($html, 200, [
+        'Content-Type' => 'text/html; charset=UTF-8',
+      ]);
+      return $response;
+    }
+    else {
+      $response = new Response();
+      $response->setContent("Block not found.");
+      $response->setStatusCode(Response::HTTP_NOT_FOUND);
+      return $response;
+    }
+  }
 
 
 }
