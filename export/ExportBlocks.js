a29136149ee5acadd757a92d4f61efb42ebe15ea
Radium Zheng <ztl8702@msn.com>
2016-06-14 15:01:57 +1000

    update documentation & clean up test page

--- export/ExportBlocks.js
+++ export/ExportBlocks.js
@@ -1,46 +1,62 @@
 (function ($){
-    "use strict";
+  "use strict";
 
-    var shadowRoot;
-    var baseURL="http://127.0.0.1:8083";
+  var shadowRoot;
+  var baseURL = "";
 
-    function getBlockURL(block) {
-        return baseURL;
-    }
-    function detectAPI() {
-        return document.body.createShadowRoot != null;
-    }
+  function getBaseURL() {
+    var pathArray = location.href.split( '/' );
+    var protocol = pathArray[0];
+    var host = pathArray[2];
+    var url = protocol + '//' + host + '/';
+    return url;
+  }
 
-    function getBlock(url, block, callback) {
-        $.ajax({
-            contentType: 'application/json',
-            method: 'GET',
-            url: baseURL + url,
-            data: {
-                '_wrapper_format': 'drupal_block',
-                'block': block
-            }
-        }).done(function(result) {
-            console.log('success');
-            //console.log(result);
-            callback(result);
-        }).fail(function(e){
-            console.log('error');
-        });
-    }
+  baseURL = getBaseURL();
 
-    function attachShadowDOM(target, url, block) {
-        getBlock(url,block, function(r){
-            shadowRoot = target.createShadowRoot();
-            shadowRoot.innerHTML = r["attachments"]["styles"]+r["attachments"]["scripts"] + r["content"] + r["attachments"]["scripts_bottom"];
-           // console.log(r.st)
-        });
-    }
+  function getBlockURL(block) {
+    return baseURL;
+  }
 
-    if (!detectAPI()) {
-        console.log("ShadowDOM is not supported by your browser!");
-    }
-    window.ExportBlocks = {
-        attachShadowDOM: attachShadowDOM
-    };
+  function detectAPI() {
+    return document.body.createShadowRoot != null;
+  }
+
+  function getBlock(url, block, callback) {
+    $.ajax({
+      contentType: 'application/json',
+      method: 'GET',
+      url: baseURL + url,
+      data: {
+        '_wrapper_format': 'drupal_block',
+        'block': block
+      }
+    }).done(function(result) {
+      console.log('success');
+      //console.log(result);
+      callback(result);
+    }).fail(function(e){
+      console.log('error');
+    });
+  }
+
+  function attachShadowDOM(target, url, block) {
+    getBlock(url,block, function(r){
+      shadowRoot = target.createShadowRoot();
+      shadowRoot.innerHTML = r["attachments"]["styles"] +
+                             r["attachments"]["scripts"] +
+                             r["content"] +
+                             r["attachments"]["scripts_bottom"];
+     // console.log(r.st)
+    });
+  }
+
+  if (!detectAPI()) {
+    console.log("ShadowDOM is not supported by your browser!");
+  }
+
+  window.ExportBlocks = {
+    attachShadowDOM: attachShadowDOM,
+    getBaseUrl: getBaseURL
+  };
 }(jQuery));
