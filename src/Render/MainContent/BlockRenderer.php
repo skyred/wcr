02d9bf7fe23f19ab27572b6c4efbba9d8be5b6fd
Radium Zheng <ztl8702@msn.com>
2016-06-14 14:37:19 +1000

    remove core patch

--- src/Render/MainContent/BlockRenderer.php
+++ src/Render/MainContent/BlockRenderer.php
@@ -7,16 +7,23 @@
 
 namespace Drupal\wcr\Render\MainContent;
 
+use Drupal\Component\Plugin\PluginManagerInterface;
 use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\InsertCommand;
+use Drupal\Core\Display\ContextAwareVariantInterface;
 use Drupal\Core\Render\AttachmentsResponseProcessorInterface;
 
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Render\HtmlResponse;
 use Drupal\Core\Render\MainContent\MainContentRendererInterface;
+use Drupal\Core\Render\PageDisplayVariantSelectionEvent;
+use Drupal\Core\Render\RenderCacheInterface;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RenderEvents;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Cache\Cache;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
@@ -24,38 +31,146 @@ use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
 /**
  * Default main content renderer for page partials.
  *
- * Currently it only prints debug info.
  */
 class BlockRenderer implements MainContentRendererInterface {
 
 
-  /**
-   * The HTML response attachments processor service.
-   *
-   * @var \Drupal\Core\Render\AttachmentsResponseProcessorInterface
-   */
-  protected static $htmlHeadAttachmentTypes = ['html_head', 'feed', 'html_head_link'];
-  protected $htmlRenderer;
 
   protected $blocks;
   protected $page_attachments;
   protected $page;
+  protected $renderer;
+  protected $displayVariantManager;
+  protected $eventDispatcher;
+  protected $renderCache;
+  protected $htmlResponseAttachmentsProcessor;
 
   /**
    * WebComponentRenderer constructor.
-   * @param MainContentRendererInterface $html_renderer
+   * @param \Drupal\wcr\Render\MainContent\PluginManagerInterface $display_variant_manager
+   * @param \Drupal\wcr\Render\MainContent\EventDispatcherInterface $event_dispatcher
+   * @param \Drupal\wcr\Render\MainContent\RenderCacheInterface $render_cache
    * @param \Drupal\Core\Render\AttachmentsResponseProcessorInterface $html_response_attachments_processor
+   * @internal param \Drupal\Core\Render\MainContent\MainContentRendererInterface $html_renderer
    */
-  public function __construct(MainContentRendererInterface $html_renderer, AttachmentsResponseProcessorInterface $html_response_attachments_processor) {
-    $this->htmlRenderer = $html_renderer;
+  public function __construct(PluginManagerInterface $display_variant_manager,
+                              EventDispatcherInterface $event_dispatcher,
+                              RenderCacheInterface $render_cache,
+                              AttachmentsResponseProcessorInterface $html_response_attachments_processor) {
     $this->renderer = \Drupal::service('renderer');
+    $this->displayVariantManager = $display_variant_manager;
+    $this->eventDispatcher = $event_dispatcher;
+    $this->renderCache = $render_cache;
     $this->htmlResponseAttachmentsProcessor = $html_response_attachments_processor;
     $this->blocks = [];
   }
 
-  protected function prepare(array $main_content, Request $request, RouteMatchInterface $route_match) {
+  /**
+   * Prepares the HTML body: wraps the main content in #type 'page'.
+   * //TODO: Remove this once HTMLRenderer is reusable.
+   *
+   * @param array $main_content
+   *   The render array representing the main content.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object, for context.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match, for context.
+   *
+   * @return array
+   *   An array with two values:
+   *   0. A #type 'page' render array.
+   *   1. The page title.
+   *
+   * @throws \LogicException
+   *   If the selected display variant does not implement PageVariantInterface.
+   */
+  public function preparePage(array $main_content, Request $request, RouteMatchInterface $route_match) {
+
+    // If the _controller result already is #type => page,
+    // we have no work to do: The "main content" already is an entire "page"
+    // (see html.html.twig).
+    if (isset($main_content['#type']) && $main_content['#type'] === 'page') {
+      $page = $main_content;
+      $title = "";
+    }
+    // Otherwise, render it as the main content of a #type => page, by selecting
+    // page display variant to do that and building that page display variant.
+    else {
+      // Select the page display variant to be used to render this main content,
+      // default to the built-in "simple page".
+      $event = new PageDisplayVariantSelectionEvent('simple_page', $route_match);
+      $this->eventDispatcher->dispatch(RenderEvents::SELECT_PAGE_DISPLAY_VARIANT, $event);
+      $variant_id = $event->getPluginId();
+
+      // We must render the main content now already, because it might provide a
+      // title. We set its $is_root_call parameter to FALSE, to ensure
+      // placeholders are not yet replaced. This is essentially "pre-rendering"
+      // the main content, the "full rendering" will happen in
+      // ::renderResponse().
+      // @todo Remove this once https://www.drupal.org/node/2359901 lands.
+      if (!empty($main_content)) {
+        $this->renderer->executeInRenderContext(new RenderContext(), function() use (&$main_content) {
+          if (isset($main_content['#cache']['keys'])) {
+            // Retain #title, otherwise, dynamically generated titles would be
+            // missing for controllers whose entire returned render array is
+            // render cached.
+            $main_content['#cache_properties'][] = '#title';
+          }
+          return $this->renderer->render($main_content, FALSE);
+        });
+        $main_content = $this->renderCache->getCacheableRenderArray($main_content) + [
+            '#title' => isset($main_content['#title']) ? $main_content['#title'] : NULL
+          ];
+      }
+
+      $title = "";
+
+      // Instantiate the page display, and give it the main content.
+      $page_display = $this->displayVariantManager->createInstance($variant_id);
+      if (!$page_display instanceof PageVariantInterface) {
+        throw new \LogicException('Cannot render the main content for this page because the provided display variant does not implement PageVariantInterface.');
+      }
+      $page_display
+        ->setMainContent($main_content)
+        ->setTitle($title)
+        ->addCacheableDependency($event)
+        ->setConfiguration($event->getPluginConfiguration());
+      // Some display variants need to be passed an array of contexts with
+      // values because they can't get all their contexts globally. For example,
+      // in Page Manager, you can create a Page which has a specific static
+      // context (e.g. a context that refers to the Node with nid 6), if any
+      // such contexts were added to the $event, pass them to the $page_display.
+      if ($page_display instanceof ContextAwareVariantInterface) {
+        $page_display->setContexts($event->getContexts());
+      }
+
+      // Generate a #type => page render array using the page display variant,
+      // the page display will build the content for the various page regions.
+      $page = array(
+        '#type' => 'page',
+      );
+      $page += $page_display->build();
+    }
+
+    // $page is now fully built. Find all non-empty page regions, and add a
+    // theme wrapper function that allows them to be consistently themed.
+    $regions = \Drupal::theme()->getActiveTheme()->getRegions();
+    foreach ($regions as $region) {
+      if (!empty($page[$region])) {
+        $page[$region]['#theme_wrappers'][] = 'region';
+        $page[$region]['#region'] = $region;
+      }
+    }
+
+    // Allow hooks to add attachments to $page['#attached'].
+    $this->invokePageAttachmentHooks($page);
+
+    return $page;
+  }
+
+  protected function prepareBlocks(array $main_content, Request $request, RouteMatchInterface $route_match) {
     // TODO: remove reliance on HTMLRenderer.
-    list($this->page, $title) = $this->htmlRenderer->prepare($main_content, $request, $route_match);
+    $this->page = $this->preparePage($main_content, $request, $route_match);
 
     // Iterate through all blocks.
     $regions = \Drupal::theme()->getActiveTheme()->getRegions();
@@ -63,7 +178,7 @@ class BlockRenderer implements MainContentRendererInterface {
       if (!empty($this->page[$region])) {
         // Non-empty region, iterate the blocks inside it.
         foreach ($this->page[$region] as $key => $child) {
-          if (substr($key,0,1) != '#') {
+          if (substr($key, 0, 1) != '#') {
             $this->blocks[$region .'/' .$key] = array (
               "id" => $region .'/' .$key,
               "render_array" => $child,
@@ -91,11 +206,15 @@ class BlockRenderer implements MainContentRendererInterface {
     $block_requested = $request->get("block");
     $is_REST = $request->getContentType() == "json";
     // Prepare.
-    $this->prepare($main_content, $request, $route_match);
+    $this->prepareBlocks($main_content, $request, $route_match);
 
     if (empty($block_requested)) {
       // List Mode
-      return $this->renderBlockList();
+      if ($is_REST) {
+        return $this->renderBlockListREST();
+      } else {
+        return $this->renderBlockList();
+      }
     } else {
       // Render Mode
       if ($is_REST) {
@@ -119,6 +238,16 @@ class BlockRenderer implements MainContentRendererInterface {
 
     $response->headers->set('Content-Type', 'text/html');
     $response->setContent($debug);
+    return $response;
+  }
+
+  protected function renderBlockListREST() {
+    //  Use a Symfony response object to have complete control over the response.
+    $response = new Response();
+    $response->setStatusCode(Response::HTTP_OK);
+    $keys = array_keys($this->blocks);
+    $response->headers->set('Content-Type', 'application/json');
+    $response->setContent(\json_encode($keys));
     return $response;
   }
 
