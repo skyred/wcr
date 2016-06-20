44972198de218735d349a6c2303e85a69887d2e6
Radium Zheng <ztl8702@msn.com>
2016-06-20 16:49:40 +1000

    Subscribe to DisplayVariant event

---
+++ src/EventSubscriber/ComponentsDisplayVariantSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\wcr\EventSubscriber\ComponentsDisplayVariantSubscriber.
+ */
+
+namespace Drupal\wcr\EventSubscriber;
+
+use Drupal\Core\Render\PageDisplayVariantSelectionEvent;
+use Drupal\Core\Render\RenderEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ComponentsDisplayVariantSubscriber implements EventSubscriberInterface {
+
+  public function onBlockPageDisplayVariantSelected(PageDisplayVariantSelectionEvent $event) {
+    // Only activate when Block is enabled.
+    if ($event->getPluginId() === 'block_page') {
+      $event->setPluginId('components_page');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    // Set a very low priority, so that it runs last.
+    $events[RenderEvents::SELECT_PAGE_DISPLAY_VARIANT][] = ['onBlockPageDisplayVariantSelected', -10000];
+    return $events;
+  }
+
+}
--- src/Plugin/DisplayVariant/ComponentsDisplayVariant.php
+++ src/Plugin/DisplayVariant/ComponentsDisplayVariant.php
@@ -6,6 +6,7 @@
  */
 
 namespace Drupal\wcr\Plugin\DisplayVariant;
+use Drupal\block\Plugin\DisplayVariant\BlockPageVariant;
 
 use Drupal\Component\Uuid\UuidInterface;
 use Drupal\Component\Utility\Html;
@@ -21,7 +22,6 @@ use Drupal\Core\Plugin\ContextAwarePluginInterface;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Utility\Token;
-use Drupal\ctools\Plugin\DisplayVariant\BlockDisplayVariant;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -32,232 +32,34 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   admin_label = @Translation("Componentized Block page")
  * )
  */
-class ComponentsDisplayVariant extends BlockDisplayVariant implements PageVariantInterface{
+class ComponentsDisplayVariant extends BlockPageVariant {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = parent::build();
 
-    /**
-     * The module handler.
-     *
-     * @var \Drupal\Core\Extension\ModuleHandlerInterface
-     */
-    protected $moduleHandler;
+    // Set default page cache keys that include the display.
+    $build['#cache']['keys'] = [
+      'components_display',
+      $this->id(),
+    ];
 
-    /**
-     * Constructs a new BlockDisplayVariant.
-     *
-     * @param array $configuration
-     *   A configuration array containing information about the plugin instance.
-     * @param string $plugin_id
-     *   The plugin ID for the plugin instance.
-     * @param mixed $plugin_definition
-     *   The plugin implementation definition.
-     * @param \Drupal\Core\Plugin\Context\ContextHandlerInterface $context_handler
-     *   The context handler.
-     * @param \Drupal\Core\Session\AccountInterface $account
-     *   The current user.
-     * @param \Drupal\Component\Uuid\UuidInterface $uuid_generator
-     *   The UUID generator.
-     * @param \Drupal\Core\Utility\Token $token
-     *   The token service.
-     * @param \Drupal\Core\Block\BlockManager $block_manager
-     *   The block manager.
-     * @param \Drupal\Core\Condition\ConditionManager $condition_manager
-     *   The condition manager.
-     * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-     *   The module handler.
-     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, ContextHandlerInterface $context_handler, AccountInterface $account, UuidInterface $uuid_generator, Token $token, BlockManager $block_manager, ConditionManager $condition_manager, ModuleHandlerInterface $module_handler) {
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $context_handler, $account, $uuid_generator, $token, $block_manager, $condition_manager);
+    foreach (Element::children($build) as $region => $blocks) {
+      $build[$region]['components_page_region_wrapper'] = ['#markup' => '<div data-components-page-region></div>'];
 
-        $this->moduleHandler = $module_handler;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-        return new static(
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            $container->get('context.handler'),
-            $container->get('current_user'),
-            $container->get('uuid'),
-            $container->get('token'),
-            $container->get('plugin.manager.block'),
-            $container->get('plugin.manager.condition'),
-            $container->get('module_handler')
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build() {
-        // Set default page cache keys that include the display.
-        $build['#cache']['keys'] = [
-            'components_display',
-            $this->id(),
+      foreach ($blocks as $key => $block) {
+        $build[$region][$key] = [
+          '#theme' => 'componentized_block',
+          '#element_name' => $region . '/' . $key,
+          '#weight' => $build[$region][$key]['#weight'],
+          '#cache' => $build[$region][$key]['#cache'],
         ];
-        $build['#pre_render'][] = [$this, 'buildRegions'];
-        return $build;
+        $build[$region][$key]['#cache']['keys'][] = ['components_display', $this->id(), 'block', $key];
+      }
     }
 
-    /**
-     * #pre_render callback for building the regions.
-     */
-    public function buildRegions(array $build) {
-        $cacheability = CacheableMetadata::createFromRenderArray($build)
-            ->addCacheableDependency($this);
-
-        $contexts = $this->getContexts();
-        foreach ($this->getRegionAssignments() as $region => $blocks) {
-            if (!$blocks) {
-                continue;
-            }
-
-            $region_name = Html::getClass("block-region-$region");
-            $build[$region]['#prefix'] = '<div class="' . $region_name . '">';
-            $build[$region]['#suffix'] = '</div>';
-
-            /** @var \Drupal\Core\Block\BlockPluginInterface[] $blocks */
-            $weight = 0;
-            foreach ($blocks as $block_id => $block) {
-                if ($block instanceof ContextAwarePluginInterface) {
-                    $this->contextHandler()->applyContextMapping($block, $contexts);
-                }
-                $access = $block->access($this->account, TRUE);
-                $cacheability->addCacheableDependency($access);
-                if (!$access->isAllowed()) {
-                    continue;
-                }
-
-                $block_build = [
-                    '#theme' => 'componentized_block',
-                    '#element_name' => $block_id,
-                    '#weight' => $weight++,
-                    '#cache' => [
-                        'keys' => ['components_display', $this->id(), 'block', $block_id],
-                        // Each block needs cache tags of the page and the block plugin, as
-                        // only the page is a config entity that will trigger cache tag
-                        // invalidations in case of block configuration changes.
-                        'tags' => Cache::mergeTags($this->getCacheTags(), $block->getCacheTags()),
-                        'contexts' => $block->getCacheContexts(),
-                        'max-age' => $block->getCacheMaxAge(),
-                    ],
-                ];
-
-                // Merge the cacheability metadata of blocks into the page. This helps
-                // to avoid cache redirects if the blocks have more cache contexts than
-                // the page, which the page must respect as well.
-                $cacheability->addCacheableDependency($block);
-
-                // If an alter hook wants to modify the block contents, it can append
-                // another #pre_render hook.
-                $this->moduleHandler->alter(['block_view', 'block_view_' . $block->getBaseId()], $block_build, $block);
-                $build[$region][$block_id] = $block_build;
-            }
-        }
-
-        $build['#title'] = $this->renderPageTitle($this->configuration['page_title']);
-
-        $cacheability->applyTo($build);
-
-        return $build;
-    }
-
-    /**
-     * #pre_render callback for building a block.
-     *
-     * Renders the content using the provided block plugin, if there is no
-     * content, aborts rendering, and makes sure the block won't be rendered.
-     */
-    public function buildBlock($build) {
-        $content = $build['#block_plugin']->build();
-        // Remove the block plugin from the render array.
-        unset($build['#block_plugin']);
-        if ($content !== NULL && !Element::isEmpty($content)) {
-            $build['content'] = $content;
-        }
-        else {
-            // Abort rendering: render as the empty string and ensure this block is
-            // render cached, so we can avoid the work of having to repeatedly
-            // determine whether the block is empty. E.g. modifying or adding entities
-            // could cause the block to no longer be empty.
-            $build = [
-                '#markup' => '',
-                '#cache' => $build['#cache'],
-            ];
-        }
-        // If $content is not empty, then it contains cacheability metadata, and
-        // we must merge it with the existing cacheability metadata. This allows
-        // blocks to be empty, yet still bubble cacheability metadata, to indicate
-        // why they are empty.
-        if (!empty($content)) {
-            CacheableMetadata::createFromRenderArray($build)
-                ->merge(CacheableMetadata::createFromRenderArray($content))
-                ->applyTo($build);
-        }
-        return $build;
-    }
-
-    /**
-     * Renders the page title and replaces tokens.
-     *
-     * @param string $page_title
-     *   The page title that should be rendered.
-     *
-     * @return string
-     *   The page title after replacing any tokens.
-     */
-    protected function renderPageTitle($page_title) {
-        $data = $this->getContextAsTokenData();
-        return $this->token->replace($page_title, $data);
-    }
-
-    /**
-     * Returns available context as token data.
-     *
-     * @return array
-     *   An array with token data values keyed by token type.
-     */
-    protected function getContextAsTokenData() {
-        $data = [];
-        foreach ($this->getContexts() as $context) {
-            // @todo Simplify this when token and typed data types are unified in
-            //   https://drupal.org/node/2163027.
-            if (strpos($context->getContextDefinition()->getDataType(), 'entity:') === 0) {
-                $token_type = substr($context->getContextDefinition()->getDataType(), 7);
-                if ($token_type == 'taxonomy_term') {
-                    $token_type = 'term';
-                }
-                $data[$token_type] = $context->getContextValue();
-            }
-        }
-        return $data;
-    }
-    /**
-     * {@inheritdoc}
-     */
-    public function setMainContent(array $main_content) {
-        $this->mainContent = $main_content;
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setTitle($title) {
-        $this->title = $title;
-        return $this;
-    }
-    /**
-     * {@inheritdoc}
-     */
-    public function getRegionNames() {
-        return [
-            'top' => 'Top',
-            'bottom' => 'Bottom',
-        ];
-    }
+    return $build;
+  }
 
 }
